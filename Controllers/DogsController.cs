822b54e5b70076359a03da2801038297b4704efc
Harrison Ledford <Harrison.Ledford@blackbaud.me>
2022-04-13 15:58:42 -0400

    Adding POST endpoint 🔥🅿🔥

---
+++ Controllers/DogsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FureverFriends.Models;
+
+namespace FureverFriends.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DogsController : ControllerBase
+{
+    private readonly ILogger<DogsController> _logger;
+
+    private dbContext _database = new dbContext();
+
+    public DogsController(ILogger<DogsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public Dog Get(int Id)
+    {
+        return _database.Dogs.Where(x => x.Id == Id).FirstOrDefault();
+    }
+
+    [HttpPost]
+    public async Task<Dog> Post(Dog dog)
+    {
+        await _database.Dogs.AddAsync(dog);
+        await _database.SaveChangesAsync();
+
+        return dog;
+    }
+}
