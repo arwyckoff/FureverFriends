f409bb3cddebc7ab9d90ccddaf8ab6b521858cdd
Harrison Ledford <Harrison.Ledford@blackbaud.me>
2022-04-13 15:32:57 -0400

    EFCore added. GitIgnore updated to exclude angular cache.

---
+++ Models/dbContext.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FureverFriends.Models
+{
+    public partial class dbContext : DbContext
+    {
+        public dbContext()
+        {
+        }
+
+        public dbContext(DbContextOptions<dbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<BuildVersion> BuildVersions { get; set; } = null!;
+        public virtual DbSet<Dog> Dogs { get; set; } = null!;
+        public virtual DbSet<ErrorLog> ErrorLogs { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=tcp:furever-server.database.windows.net,1433;Initial Catalog=Furever-Database;Persist Security Info=False;User ID=furever-admin;Password=FurBase321!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BuildVersion>(entity =>
+            {
+                entity.HasKey(e => e.SystemInformationId)
+                    .HasName("PK__BuildVer__35E58ECA80843EAD");
+
+                entity.ToTable("BuildVersion");
+
+                entity.Property(e => e.SystemInformationId)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("SystemInformationID");
+
+                entity.Property(e => e.DatabaseVersion)
+                    .HasMaxLength(25)
+                    .HasColumnName("Database Version");
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.VersionDate).HasColumnType("datetime");
+            });
+
+            modelBuilder.Entity<Dog>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<ErrorLog>(entity =>
+            {
+                entity.ToTable("ErrorLog");
+
+                entity.Property(e => e.ErrorLogId).HasColumnName("ErrorLogID");
+
+                entity.Property(e => e.ErrorMessage).HasMaxLength(4000);
+
+                entity.Property(e => e.ErrorProcedure).HasMaxLength(126);
+
+                entity.Property(e => e.ErrorTime)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.UserName).HasMaxLength(128);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
