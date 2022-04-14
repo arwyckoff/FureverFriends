8b3af0ef27127a5b380a7fd38da0d44489c1abda
Tina Conway <tina.conway@blackbaud.com>
2022-04-14 15:05:32 -0400

    feat: add endpoints for dog crud

--- Services/DogsService.cs
+++ Services/DogsService.cs
@@ -12,11 +12,59 @@ public class DogService
 
     public Dog GetDog(int Id)
     {
-      return _database.Dogs.Where(x => x.Id == Id).FirstOrDefault();
+      var dog = _database.Dogs.Where(x => x.Id == Id).FirstOrDefault();
+      if (dog == null)
+      {
+        throw new InvalidOperationException("Cannot find dog");
+      }
+      else
+      {
+        return dog;
+      }
     }
 
     public List<Dog> GetDogs()
     {
       return _database.Dogs.ToList();
+    }
+
+  
+    public List<Dog> SearchDogs(string term)
+    {
+      return _database.Dogs.Where(dog => dog.Name == term || dog.Name.Contains(term)).ToList();
+    }
+
+    public async Task<Dog> CreateOrUpdateDog(Dog dog)
+    {
+      bool isUpdate = _database.Dogs.Any(x => x.Id == dog.Id);
+      if (isUpdate)
+      {
+        _database.Dogs.Update(dog);
+        await _database.SaveChangesAsync();
+      }
+      else
+      {
+        await _database.Dogs.AddAsync(dog);
+        await _database.SaveChangesAsync();
+      }
+
+      return dog;
+    }
+
+    public async Task<int> DeleteDog(int Id)
+    {
+      var dog = _database.Dogs.Where(x => x.Id == Id).FirstOrDefault();
+      if (dog != null)
+      {
+        _database.Dogs.Remove(dog);
+        await _database.SaveChangesAsync();
+
+        return Id;
+      }
+      else
+      {
+        throw new InvalidOperationException("Cannot find dog");
+      }
+
     }
 }
